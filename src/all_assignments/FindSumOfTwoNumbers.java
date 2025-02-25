e8a847bfc8e016a21878a885d7f962d2535e3cde
Pankaj Bhardwaj <pankaj.bhardwaj@rx.local>
2025-02-25 17:57:33 +0530

    day07

---
+++ src/all_assignments/FindSumOfTwoNumbers.java
@@ -0,0 +1,21 @@
+package all_assignments;
+
+public class FindSumOfTwoNumbers {
+
+	public static void main(String[] args) {
+		// Find sum of two numbers.
+		
+		int a = 10;
+		int b = 20;
+		
+		System.out.println("Sum Of a and b is: "+ (a+b));
+		System.out.println("Sum Of a and b is: "+ a+b); // without braces.
+		
+		// Multiplication.
+		System.out.println("Multiplication of a and b is :"+ a*b);
+		System.out.println("Multiplication of a and b is :"+ (a*b));
+		
+		
+	}
+
+}
---
+++ src/day03/Assignment01SwappingOfVariableUsingTempVariable.java
@@ -0,0 +1,10 @@
+package day03;
+
+public class Assignment01SwappingOfVariableUsingTempVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
