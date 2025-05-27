201547139087a9a06f55e4266b8a3a565442f59e
pankaj379 <pankajkundal777@gmail.com>
2025-05-27 23:28:38 +0530

    collections

--- src/day20/Hashset_Demo.java
+++ src/day20/Hashset_Demo.java
@@ -1,6 +1,8 @@
 package day20;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class Hashset_Demo {
@@ -9,7 +11,7 @@ public class Hashset_Demo {
 		// Hashset collections.
 		
 		HashSet hs = new HashSet();
-		//Set st = new HashSet();  // Set is a parent claas.
+		//Set hs = new HashSet();  // Set is a parent claas.
 		//HashSet<String> name = new HashSet<String>(); // for homogenious data.
 		
 		
@@ -24,8 +26,46 @@ public class Hashset_Demo {
 		hs.add(1004284.323242);
 		hs.add(null);
 		
+		
 		System.out.println("Hashset values : "+ hs); // [null, Java and Selenium, 100, J, 198.09, 1004284.323242, true]
 
+		// Size of hashset.
+		System.out.println("Size of Hashset : "+hs.size()); // 7
+		
+		// Remove value from hashset.
+		hs.remove(100); // directly removed the value.(Not index)
+		System.out.println("After removing value : "+ hs); // [null, Java and Selenium, J, 198.09, 1004284.323242, true]
+	
+		// Inserting Elemet - not possible becuase it's not in proper order like array.
+		
+		// Access specific element is also not possible.
+		// But alternative way is we can user arraylist to access specific element.
+		// For interview the answer is clearly no for access element directly but alternatively by converting hashset into array list.
+		
+		// Convert Hashset into arraylist.
+		ArrayList al = new ArrayList(hs);
+		System.out.println(al.get(1)); // Java and Selenium
+		
+		// Read data from hashset.
+		// Note only possible with enhanced for loop not with simple for loop because indexing is needed for normal for loop.
+		for(Object h:hs) {
+			System.out.print(h);  // nullJava and SeleniumJ198.091004284.323242true
+		}
+		
+		
+		// By using Iterator.
+		Iterator<Object> it = hs.iterator();
+		while(it.hasNext()) {
+			System.out.println(it.next()); // with ln loop run with line space.
+		}
+		
+		
+		// Remove all elements from the hashset.
+		hs.clear();
+		System.out.println("After clearing all elements :"+hs); // []
+		System.out.println(hs.isEmpty()); // true
+		
+		
+		
 	}
-
 }
